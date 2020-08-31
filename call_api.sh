42e8c67020fed4c9b804b0087ba29a4148e84d08
Hideaki Tanabe <tanablog@gmail.com>
2020-08-31 23:37:16 +0900

    Add locale parameter

--- call_api.sh
+++ call_api.sh
@@ -24,18 +24,21 @@ fi
 
 case $1 in
 cards)
-    if [ -z "$2" ]; then
+    if [ -z "$3" ]; then
         echo "Please specify card id"
         exit 1
     fi
 
-    CARD_ID=$2
+    LOCALE=$2
+    CARD_ID=$3
     curl --header "Authorization: Bearer ${ACCESS_TOKEN}" ${API_ENDPOINT}/cards/${CARD_ID}?locale=${LOCALE}
     ;;
 metadata)
+    LOCALE=$2
     curl --header "Authorization: Bearer ${ACCESS_TOKEN}" ${API_ENDPOINT}/metadata/classes?locale=${LOCALE}
     ;;
 deck)
+    LOCALE=$2
     DECK_CODE="AAECAR8etQPhBIoHlwjbCfyjA+SkA6alA/KlA4SnA4qtA4utA/muA/yvA/6vA4ewA+ewA/ewA/+wA4exA4ixA5GxA9iyA6K3A6+3A4O5A6K5A/+6A9e+A+W+AwAA"
     curl --header "Authorization: Bearer ${ACCESS_TOKEN}" ${API_ENDPOINT}/deck/${DECK_CODE}?locale=${LOCALE}
     ;;
